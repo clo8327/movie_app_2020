1b393cf31212d1b549cefa19e870a5704f8e17e2
Davidjwkim <clo8327@gmail.com>
2020-10-12 13:30:32 +0900

    Updates

---
+++ precache-manifest.c6d0590da18d3be84d5947c2b1f32ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a6ce541d8579fd93bce17669365c47",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1ed95c9cd1f8c4ad59ae",
+    "url": "/movie_app_2020/static/css/main.d41c0b3a.chunk.css"
+  },
+  {
+    "revision": "a7d22b2c00d8d326c863",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed95c9cd1f8c4ad59ae",
+    "url": "/movie_app_2020/static/js/main.9e739810.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
